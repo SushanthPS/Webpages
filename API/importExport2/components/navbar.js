91e5e618ca1393317250e02c8e881000ac29eca5
Sushanth PS <35593841+SushanthPS@users.noreply.github.com>
2021-10-25 21:41:56 +0530

    Add files via upload

---
+++ API/importExport2/components/navbar.js
@@ -0,0 +1,10 @@
+function navbar() {
+    return `<div><img src="https://dcassetcdn.com/design_img/10150/1674/1674_291293_10150_image.jpg" alt=""></div>
+    <div id="navbar-pages">
+        <div onclick="location.href='searchPage.html'">Search</div>
+        <div onclick="location.href='randomRecipe.html'">Random Recipe</div>
+        <div onclick="location.href='latestRecipe.html'">Latest Recipes</div>
+    </div>`
+}
+
+export default navbar
